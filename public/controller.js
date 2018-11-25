6590aee785ee7fb7cbee749f26082eb4e27e2d65
Rdubya54 <ryan.wortmann@yahoo.com>
2018-11-25 16:10:39 -0600

    angular can now read AND write to firebase

--- public/controller.js
+++ public/controller.js
@@ -11,31 +11,65 @@ var config = {
 
 firebase.initializeApp(config);
 
-angular
-    .module('app',['firebase'])
-    .controller('MyCtrl', function($firebaseObject,$scope){
+var app=angular.module('app',['firebase'])
+    app.controller('MyCtrl', function($firebaseArray,$scope){
 
         $scope.something="hiii";
-        $scope.time="";
+        // $scope.time="";
 
-        const db=firebase.firestore();
-
-        const productsRef=db.collection('test_database');
-
-        const query = productsRef.where('price', '>=', 10)
-        console.log("dumb")
-        query.get()
-            .then(products => {
-                products.forEach(doc =>{
-                    data=doc.data()
-                    console.log("in here")
-                    console.log(data.time)
-                    $scope.time=data.time;
-                    // document.write(data.time + ' at $' + data.time +'<br>')
-                })   
-            
-            }) 
         
+
+        // const db=firebase.firestore();
+
+        // const productsRef=db.collection('test_database');
+
+        // const query = productsRef.where('price', '>=', 10)
+        // console.log("dumb")
+        // query.get()
+        //     .then(products => {
+
+        //         const list=[];
+
+        //         products.forEach(doc =>{
+        //             data=doc.data()
+        //             console.log("in here")
+        //             console.log(data.time)
+        //             $scope.time=data.time;
+        //             list.push(data.time);
+        //             console.log(list);
+        //             // document.write(data.time + ' at $' + data.time +'<br>')
+        //         })   
+        //         return list;
+            
+        //     }) 
+        // $scope.syncArray=list;
+
+        function list(user) {
+            return firebase.firestore()
+              .collection('test_database')
+              .get()
+              .then(snapshot => {
+                const list = [];
+          
+                snapshot.forEach(doc => {
+                  const data = doc.data()
+                  data.id = doc.price;
+                  list.push(data);
+                });
+          
+                return list;
+              })
+              .catch(err => {
+                console.log('Error getting documents', err);
+              });
+          }
+
+        list({ uid: 10 }).then((list) => {
+            console.log(list);
+            $scope.time=list;
+            console.log($scope.time);
+            $scope.$apply()
+          });
             
 
         // const rootRef=firebase.database().ref().child('angular');
@@ -43,6 +77,23 @@ angular
         // const ref=rootRef.child('object');
         // console.log(ref)
         // this.object=$firebaseObject(ref);
+
+        $scope.googleLogin=function(){
+            const provider = new firebase.auth.GoogleAuthProvider();
+        
+            firebase.auth().signInWithPopup(provider)
+        
+                .then(result =>{
+                    const user=result.user;
+                    //document.write('Hello ' + user.displayName);
+                    window.location='homepage.html';
+                    console.log(user)
+                })
+                .catch(console.log)
+
+        }
+
+
     });
 
 }());
